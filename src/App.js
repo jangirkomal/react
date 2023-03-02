898f2395d3ab1fca85e7c0530c4f46afb7b894d8
komaljangir177 <komaljangir177@gmail.com>
2023-03-02 15:32:43 +0530

    react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useState} from 'react';
+
 function App() {
+
+  const [name,setname]=useState("")
+  const [f_name,f_setname]=useState("")
+  const [m_name,m_setname]=useState("")
+  const [j_jaipur,j_setname]=useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+
+   <div class="container">
+    <div class="blog">
+    <p>my name is {name}   </p>
+    <div>
+     <label for="">name</label>
+      <input type="text" placeholder="enter your name"  onChange={(e)=>setname(e.target.value)}/>
     </div>
+    <div >
+    <p>my f_name is {f_name}</p>
+      <label for="">f_name</label>
+      <input type="text" placeholder="enter your f_name" onChange={(e)=>f_setname(e.target.value)}/>
+    </div>
+    <div>
+    <p>my m_name is {m_name}</p>
+      <label for="">M_name</label>
+      <input type="text" placeholder="enter your M_name" onChange={(e)=>m_setname(e.target.value)}/>
+    </div>
+    <div>
+   <p> my address is {j_jaipur}</p>
+      <label for="">address</label>
+      <input type="text" placeholder="enter your address" onChange={(e)=>j_setname(e.target.value)}/>
+    </div>
+   </div>
+</div>
   );
 }
 
